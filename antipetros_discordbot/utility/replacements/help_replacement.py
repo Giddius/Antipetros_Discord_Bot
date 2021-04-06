386b0a881339658ea48193604d583e4fa7c6dc57
Giddius <GordonGrossmann@gmx.at>
2021-04-06 23:15:04 +0200

    missed that

--- antipetros_discordbot/utility/replacements/help_replacement.py
+++ antipetros_discordbot/utility/replacements/help_replacement.py
@@ -16,20 +16,3 @@ COGS_CONFIG = ParaStorageKeeper.get_config('cogs_config')
 
 
 class BaseCustomHelpCommand(MinimalHelpCommand):
-    bot = None
-    mention_placeholder = "@BOTMENTION"
-
-    def __init__(self, sort_commands: bool = True, **options):
-        super().__init__(sort_commands=sort_commands, **options)
-
-    def get_command_signature(self, command):
-        if hasattr(command, 'example') and command.example is not None:
-            return f"```css\n{command.example.replace(self.mention_placeholder,'@'+ self.bot.display_name)}\n```"
-        return super().get_command_signature(command)
-
-    async def send_command_help(self, command):
-        await super().send_command_help(command)
-        if hasattr(command, "gif") and command.gif is not None:
-            file = discord.File(command.gif)
-            destination = self.get_destination()
-            await destination.send(file=file)
