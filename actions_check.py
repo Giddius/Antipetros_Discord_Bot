0bceb2b89b758eaa260c449da298e5a65015f6a8
Giddius <GordonGrossmann@gmx.at>
2021-05-15 19:44:29 +0200

    changed template

--- actions_check.py
+++ actions_check.py
@@ -2,6 +2,8 @@ import os
 
 
 with open('actions_check_output.txt', 'w') as f:
-    for file in os.scandir(os.getcwd()):
-        f.write(f"- '{file.path}'")
-        f.write('\n\n')
+    for dirname, folderlist, filelist in os.walk(os.getcwd()):
+        for file in filelist:
+            path = os.path.relpath(os.path.join(dirname, file))
+            f.write(f"- '{path}'")
+            f.write('\n')
